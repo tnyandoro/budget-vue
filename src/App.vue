9a2b87ddc5dbce7503df1edc341e1fdebfe6b259
Tendai <tnyandoro@gmail.com>
2022-10-23 18:18:10 +0200

    Create delete method for task

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks :tasks="tasks" />
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
   </div>
 
 </template>
@@ -19,6 +19,12 @@ export default {
   data() {
     return {
       tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      console.log('task', id)
+      // this.tasks = this.tasks.filter((task) => task.id !== id)
     }
   },
   created() {
