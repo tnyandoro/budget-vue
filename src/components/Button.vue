1be9d37d62948ebb9bd32b0cc4ba1258c30e8925
Tendai <tnyandoro@gmail.com>
2022-10-23 12:46:48 +0200

    Add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <button>Add Task</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <h1>{{ title}}</h1>
+        <Button />
+    </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+        },
+        components: {
+            Button
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+ header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 20px;
+ }
+</style>
